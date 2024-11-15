108766198cb2358b21b1f76b9fce549e152c0f56
reign021b <reign021b@gmail.com>
2024-11-15 10:44:06 +0800

    modified navigation buttons styles

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { supabase } from "/utils/supabase/client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
 
@@ -15,12 +15,15 @@ import { FaSignOutAlt } from "react-icons/fa";
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.push("/login");
   };
+
+  const isActive = (path) => router.pathname === path;
 
   return (
     <div className="flex items-center justify-between px-10 py-3">
@@ -39,35 +42,71 @@ const Navbar = () => {
 
       <div className="flex justify-between">
         <Link href="/">
-          <div className="flex items-center mr-2 hover:bg-green-50 hover:text-green-600 p-3 rounded-lg cursor-pointer">
+          <div
+            className={`flex items-center mr-2 p-3 rounded-lg cursor-pointer ${
+              pathname === "/"
+                ? "bg-green-50 text-green-600"
+                : "hover:bg-green-50 hover:text-green-600"
+            }`}
+          >
             <AiOutlineHome className="text-2xl" />
             <p className="font-semibold text-sm ml-2">Dashboard</p>
           </div>
         </Link>
 
         <Link href="/tracking">
-          <div className="flex items-center mr-2 hover:bg-green-50 hover:text-green-600 p-3 rounded-lg group">
-            <TiLocationArrowOutline className="text-2xl border-2 rounded-full border-black font-bold group-hover:border-green-600" />
+          <div
+            className={`flex items-center mr-2 p-3 rounded-lg group ${
+              pathname === "/tracking"
+                ? "bg-green-50 text-green-600"
+                : "hover:bg-green-50 hover:text-green-600"
+            }`}
+          >
+            <TiLocationArrowOutline
+              className={`text-2xl border-2 rounded-full font-bold ${
+                pathname === "/tracking"
+                  ? "border-green-600"
+                  : "border-black group-hover:border-green-600"
+              }`}
+            />
             <p className="font-semibold text-sm ml-2">Tracking</p>
           </div>
         </Link>
 
         <Link href="/inventory">
-          <div className="flex items-center mr-2 hover:bg-green-50 hover:text-green-600 p-3 rounded-lg group">
+          <div
+            className={`flex items-center mr-2 p-3 rounded-lg group ${
+              pathname === "/inventory"
+                ? "bg-green-50 text-green-600"
+                : "hover:bg-green-50 hover:text-green-600"
+            }`}
+          >
             <MdOutlineInventory className="text-2xl" />
             <p className="font-semibold text-sm ml-2">Inventory</p>
           </div>
         </Link>
 
         <Link href="/shipment">
-          <div className="flex items-center mr-2 hover:bg-green-50 hover:text-green-600 p-3 rounded-lg group">
+          <div
+            className={`flex items-center mr-2 p-3 rounded-lg group ${
+              pathname === "/shipment"
+                ? "bg-green-50 text-green-600"
+                : "hover:bg-green-50 hover:text-green-600"
+            }`}
+          >
             <BsTruck className="text-2xl transform scale-x-[-1]" />
             <p className="font-semibold text-sm ml-2">Shipment</p>
           </div>
         </Link>
 
         <Link href="#">
-          <div className="flex items-center mr-2 hover:bg-green-50 hover:text-green-600 p-3 rounded-lg group">
+          <div
+            className={`flex items-center mr-2 p-3 rounded-lg group ${
+              pathname === "#"
+                ? "bg-green-50 text-green-600"
+                : "hover:bg-green-50 hover:text-green-600"
+            }`}
+          >
             <MdOutlineBarChart className="text-xl border-2 rounded border-black font-bold group-hover:border-green-600" />
             <p className="font-semibold text-sm ml-2">Finances</p>
           </div>
@@ -93,18 +132,18 @@ const Navbar = () => {
       {/* Logout Confirmation Modal */}
       {showLogoutModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-5 rounded-lg shadow-lg text-center w-80">
+          <div className="bg-white p-5 rounded-2xl shadow-lg text-center w-80">
             <p className="text-lg font-semibold mb-4">Confirm Logout</p>
-            <p className="mb-6">Are you sure you want to log out?</p>
+            <p className="mb-6">Do you want to log out?</p>
             <div className="flex justify-center space-x-4">
               <button
-                className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600"
+                className="bg-red-500 text-white py-2 px-4 rounded-xl hover:bg-red-600"
                 onClick={handleSignOut}
               >
-                Confirm
+                Logout
               </button>
               <button
-                className="bg-gray-300 py-2 px-4 rounded-lg hover:bg-gray-400"
+                className="bg-gray-300 py-2 px-4 rounded-xl hover:bg-gray-400"
                 onClick={() => setShowLogoutModal(false)}
               >
                 Cancel
