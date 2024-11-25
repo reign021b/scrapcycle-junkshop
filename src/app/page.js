d83a6f96237a6c7db536356df3171ad17cd22d23
reign021b <reign021b@gmail.com>
2024-11-25 14:35:32 +0800

    modified dashboard widgets

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <main>
         <Navbar />
-        <div className="w-full bg-[#F5F5F5] h-[91vh] mx-auto">
+        <div className="w-full bg-[#F5F5F5] h-auto mx-auto pb-6">
           <div className="max-w-[1340px] mx-auto">
             <p className="text-[1.6rem] font-semibold pt-5 pl-8">
               Business Overview
@@ -14,7 +14,7 @@ export default function Home() {
               Junkshop Dashboard
             </p>
           </div>
-          <div className="max-w-[1340px] mx-auto flex gap-6 text-sm font-medium">
+          <div className="max-w-[1340px] mx-auto flex gap-[1.75rem] text-sm font-medium">
             {/* 1st column */}
             <div>
               {/* 1st row */}
@@ -37,12 +37,27 @@ export default function Home() {
               <div className="flex items-center justify-center mt-6 border rounded-xl bg-white w-[972px] h-[303px]">
                 Sales vs Cost vs Profit
               </div>
+
+              <div className="flex mt-6 gap-[1.75rem]">
+                <div className="w-[472px] h-[363px] flex items-center justify-center rounded-xl border bg-white">
+                  Cost
+                </div>
+                <div className="w-[472px] h-[363px] flex items-center justify-center rounded-xl border bg-white">
+                  Scraps Market Prices
+                </div>
+              </div>
             </div>
 
             {/* 2nd column */}
             <div className="w-[340px] mt-6">
+              {/* 1st row */}
               <div className="border rounded-xl bg-white h-[385px] w-[340px] flex items-center justify-center">
                 Top Sales Items
+              </div>
+
+              {/* 2nd row */}
+              <div className="border rounded-xl bg-white h-[483px] w-[340px] flex items-center justify-center mt-6">
+                Top Materials
               </div>
             </div>
           </div>
